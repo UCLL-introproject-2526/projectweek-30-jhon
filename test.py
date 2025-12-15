ed67294c058967cc977953842560b4ba72ced5e6
Ayman Alsharour <ayman.alshahrour@student.ucll.be>
2025-12-15 14:35:34 +0100

    circel tekenen

--- test.py
+++ test.py
@@ -1,21 +1,29 @@
 import pygame
 
-
+x = 1000
+y = 800
 def create_main_surface():
-    surface = pygame.display.set_mode((1000, 600))
+    surface = pygame.display.set_mode((x,y))
     return surface
+
+
+def render_frame(surface):
+    pygame.draw.circle(surface, (255, 0, 0), (x/2, y/2), 40)
+    pygame.display.flip()
 
 
 def main():
     pygame.init()
 
-    main_surface = create_main_surface()
+    surface = create_main_surface()
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        render_frame(surface)
 
     pygame.quit()
 
