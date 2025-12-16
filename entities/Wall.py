fc7dd6f46cb2ae3e06d7966ede7830c43989765b
Nick Decoster <nick.decoster@student.ucll.be>
2025-12-16 13:20:05 +0100

    wall update

--- entities/Wall.py
+++ entities/Wall.py
@@ -1,5 +1,4 @@
-import Entity
-
+from entities.Entity import Entity
 class Wall(Entity):
     def __init__(self, x, y, width, height):
         super().__init__(x, y, width, height, solid=True , texture="player/eyes_open.png")
