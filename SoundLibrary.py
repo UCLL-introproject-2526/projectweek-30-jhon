33fc0508c53601a484d61ab65783719fe8c1cc07
Wannes Geeroms <wannes.geeroms@student.ucll.be>
2025-12-16 10:49:03 +0100

    SoundLibrary (started)

--- SoundLibrary.py
+++ SoundLibrary.py
@@ -1,2 +1,11 @@
 import pygame
 
+# def play_sound(sound):
+#     sfx = pygame.mixer.Sound(f'assets/audio/{sound}')
+#     pygame.mixer.Sound.play()
+
+class SoundLibrary:
+    def __init__(self):
+        self.sfx = {
+            'footstep': 'sfx/footstep.wav',
+        }
