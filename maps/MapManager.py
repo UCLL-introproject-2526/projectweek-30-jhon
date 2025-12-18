8ec7812afdff2900e6b5c44502ca908214489cdf
Ayman Alsharour <ayman.alshahrour@student.ucll.be>
2025-12-18 11:22:35 +0100

    zonder zip

--- maps/MapManager.py
+++ maps/MapManager.py
@@ -3,7 +3,7 @@ from entities.Player import Player
 
 class MapManager:
     def __init__(self, main):
-        self.__current_map = 1
+        self.__current_map = 0
         self.__maps = build_maps(main)
         self.__player1 = Player(0, 0, main, "p1")
         self.__player2 = Player(1, 0, main, "p2")
@@ -22,7 +22,7 @@ class MapManager:
         self.get_current_map().remove_entity(self.__player2)
         self.__current_map += 1
         if self.__current_map >= len(self.__maps):
-            self.__current_map = 0
+            self.__current_map = 1
         self.get_current_map().add_entity(self.__player1)
         self.get_current_map().add_entity(self.__player2)
         self.__player1.reset_movement()
