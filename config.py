99832ecf46818192cc09de9219d251d32993060a
Wannes Geeroms <wannes.geeroms@student.ucll.be>
2025-12-18 16:27:22 +0100

    settings menu

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ from maps.MainMenu import MainMenu
 from maps.Map1 import Map1
 from maps.Map2 import Map2
 from maps.Map3 import Map3
+from maps.Settings import Settings
 
 
 def build_maps(main):
@@ -11,9 +12,10 @@ def build_maps(main):
     map1 = Map1(main)
     map2 = Map2(main)
     map3 = Map3(main)
+    settings= Settings(main)
     maps.append(main_menu)
     maps.append(map1)
     maps.append(map2)
     maps.append(map3)
-
+    maps.append(settings)
     return maps
---
+++ maps/Settings.py
@@ -0,0 +1,26 @@
+from maps.Map import Map
+from entities.Wall import Wall
+from entities.TextEntity import TextEntity
+from entities.Button import Button
+
+class Settings(Map):
+    def __init__(self, main):
+        super().__init__("settings", 500, 250, 40, 130, 335, 130, no_player=True)
+        self.setup(main)
+
+    def setup(self, main):
+        self.add_entity(Wall(0, 240, 400, 10, main))
+
+
+
+        # self.add_entity(Button(200, 110, main, None, "Play", (0, 255, 0), (0, 0, 0), "start"))
+        # self.add_entity(Button(200, 140, main, None, "Quit", (0, 255, 0), (0, 0, 0), "quit"))
+
+    def update_map(self, past_time, events):
+        pass
+        # if self.get_entity_by_id('start').is_clicked():
+        #     self.get_entity_by_id('start').main.next_map()
+        #
+        # if self.get_entity_by_id('quit').is_clicked():
+        #     self.get_entity_by_id('quit').main.quit()
+
