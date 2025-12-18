e2ee4cbbc1ed4bba8f796e9a828d899a39b2cec8
Tjorre Van Eylen <Tjorre.vaneylen@student.ucll.be>
2025-12-18 19:56:37 +0100

    Map1 optimization

--- maps/Map1.py
+++ maps/Map1.py
@@ -4,7 +4,7 @@ from entities.Button import Button
 
 class Map1(Map):
     def __init__(self, main):
-        super().__init__('map1', 400, 250, 20, 180, 360, 180)
+        super().__init__('map1', 400, 250, 30, 180, 355, 180)
         self.setup(main)
 
     def setup(self, main):
@@ -12,10 +12,13 @@ class Map1(Map):
         self.add_entity(Wall(0, 0, 12, 250, main))
         self.add_entity(Wall(388, 0, 12, 250, main))
 
-        self.add_entity(Wall(12, 224, 16, 23, main))
-        self.add_entity(Wall(372, 224, 16, 23, main))
+        self.add_entity(Wall(12, 224, 10, 12, main))
+        self.add_entity(Wall(12, 236, 16, 23, main))
+        self.add_entity(Wall(378, 224, 10, 12, main))
+        self.add_entity(Wall(372, 236, 16, 23, main))
 
 
-        self.add_entity(Wall(99, 180, 30, 67, main))
+
+        self.add_entity(Wall(100, 180, 30, 67, main))
         self.add_entity(Wall(184, 160, 42, 87, main))
-        self.add_entity(Wall(279, 180, 30, 67, main))
+        self.add_entity(Wall(280, 180, 30, 67, main))
