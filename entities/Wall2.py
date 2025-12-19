892f7388ec7cc7de636c0d78e4f32890cdc047c8
Ayman Alsharour <ayman.alshahrour@student.ucll.be>
2025-12-19 11:41:07 +0100

    s

--- entities/Wall2.py
+++ entities/Wall2.py
@@ -2,3 +2,4 @@ from entities.Entity import Entity
 class Wall2(Entity):
     def __init__(self, x, y, width, height, main):
         super().__init__(x, y, width, height, main, solid=True, texture="wall/platform")
+        
